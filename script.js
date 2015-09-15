85c52452641ceb08589c0b3880aa3fa4195b1ff2
vine-stone <l.piedra19@gmail.com>
2015-09-14 20:39:08 -0700

    overall small changes
    general cleanup, processing may not work at all embedded

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.getElementById("navi").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>Home</a></li>" +
 "<li><a href='research.html'>Research</a></li>" +
-"<li><a href='processing.html'>Projects</a></li>" +
+"<li><a href='projects.html'>Projects</a></li>" +
 "<li><a href='about.html'>Find Me</a></li>" +
 "</ul>";
