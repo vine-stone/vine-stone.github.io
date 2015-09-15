153fc1bc98a700c317771c67acad1d8565e91466
vine-stone <l.piedra19@gmail.com>
2015-09-14 20:18:03 -0700

    Tabs and looks
    pages for different topics, visual changes

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("navi").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='research.html'>Research</a></li>" +
+"<li><a href='processing.html'>Projects</a></li>" +
+"<li><a href='about.html'>Find Me</a></li>" +
+"</ul>";
